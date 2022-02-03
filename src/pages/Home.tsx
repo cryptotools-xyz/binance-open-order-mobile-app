3f9a4f132949818ac4d03ec3e1be5cb2ec7bb7cd
Souhail Merroun <souhail.merroun1996@gmail.com>
2022-02-03 08:47:47 +0100

    Update texts

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,15 +7,10 @@ const Home: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Binance open order</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
+      <IonContent>
         <ExploreContainer />
       </IonContent>
     </IonPage>
